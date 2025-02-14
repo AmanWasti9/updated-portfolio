0a561afba3632992bade511f839eb1d77ed6c4af
AmanWasti9 <wastiaman123@gmail.com>
2025-02-14 23:12:38 +0500

    new updates on app.js and particles animated bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
 import AllProjects from "./components/AllProjects";
 import Home from "./Pages/Home";
 import Contact from "./components/Contact";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top when location changes
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
       <ThemeProvider>
+        <ScrollToTop />
         <div
           className="min-h-screen bg-white dark:bg-black text-gray-900 dark:text-white"
           style={{
